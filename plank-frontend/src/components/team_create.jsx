d7de2cf94f6ff6b27d07e71f898803b51b6c7241
ygunk <ygunkun1015@gmail.com>
2026-03-18 09:57:35 +0900

    팀 가입 페이지 생성

--- plank-frontend/src/components/team_create.jsx
+++ plank-frontend/src/components/team_create.jsx
@@ -6,7 +6,9 @@ import { useNavigate } from "react-router-dom";
 import Backbtn from '../assets/back-button.svg';
 import logo from '../assets/logo.svg';
 
-const GlobalStyle = createGlobalStyle`
+
+//css
+export const GlobalStyle = createGlobalStyle`
     *{
         font-family: Pretendard;
         margin : 0;
@@ -15,21 +17,21 @@ const GlobalStyle = createGlobalStyle`
         background-color: #FFF;
     }
 `;
-
-const Container = styled.div`
+export const Container = styled.div`
     margin-top: 40px;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
 `;
-
-const Form = styled.form`
-
+export const Form = styled.form`
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
 `;
-
-const TeamNameInput = styled.input`
-    margin: 40px;
+export const TeamNameInput = styled.input`
     display: flex;
     width: 538px;
     height: 60px;
@@ -40,9 +42,30 @@ const TeamNameInput = styled.input`
     border-radius: 12px;
     background: #FFF;
     box-shadow: 0 0 11.9px 2px rgba(0, 0, 0, 0.09);
-`;
+    border: none;
 
-const Title = styled.span`
+    &:hover{
+        box-shadow: 0 0 30px 2px rgba(192, 218, 88, 0.30);
+    }
+`;
+const DateInput = styled.input`
+    display: flex;
+    width: 538px;
+    height: 60px;
+    padding: 32px 24px;
+    align-items: center;
+    gap: 10px;
+
+    border-radius: 12px;
+    background: #FFF;
+    box-shadow: 0 0 11.9px 2px rgba(0, 0, 0, 0.09);
+    border: none;
+
+    &:hover{
+        box-shadow: 0 0 30px 2px rgba(192, 218, 88, 0.30);
+    }
+`;
+export const Title = styled.span`
     margin: 40px;
     color: #959794;
     font-family: Pretendard;
@@ -51,8 +74,21 @@ const Title = styled.span`
     font-weight: 600;
     line-height: normal;
 `;
+export const SumbitButton = styled.button`
+    display: flex;
+    width: 538px;
+    height: 80px;
+    padding: 31px 232px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
 
-const BackButton = styled.button`
+    border-radius: 12px;
+    background: #C0DA58;
+    box-shadow: 0 0 29.5px 2px rgba(0, 0, 0, 0.08);
+    border: none;
+`;
+export const BackButton = styled.button`
     width: 96px;
     height: 100%;
     background: none;
@@ -62,13 +98,11 @@ const BackButton = styled.button`
     align-items: center;
     justify-content: center;
 `;
-
-const Icon = styled.img`
+export const Icon = styled.img`
     width: 32px;
     height: 64px;
 `;
-
-const Logo = styled.img`
+export const Logo = styled.img`
     width: 324px;
     height: 136px;
 `;
@@ -88,7 +122,9 @@ export default function TeamCreate(){
                     <Logo src={logo} />
                     <Title>생성하기</Title>
                     <Form>
-                        <TeamNameInput />
+                        <TeamNameInput placeholder="이름" />
+                        <DateInput placeholder="일정 종료 일" />
+                        <SumbitButton>생성하기</SumbitButton>
                     </Form>
                 </Container>
         </>
