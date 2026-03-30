726f8e548f86c6d7f6b897d283ae5ae331371f1c
08april06 <hujab0406@gmail.com>
2026-03-30 10:00:48 +0900

    사이드 바 구현, 로그인 회원가입

--- plank-frontend/src/App.jsx
+++ plank-frontend/src/App.jsx
@@ -2,20 +2,36 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import TeamCreate from "./components/team_create";
 import Login from "./pages/login";
-import HomePage  from "./pages/homePage";
+import HomePage from "./pages/homePage";
+import Schedule from "./components/schedule";
+import Signup from "./pages/signup";
+import Project from "./components/project";
+import Chat from "./components/chat";
+import MyPage from "./components/mypage";
+
+/* 🔥 추가 */
+import Alarm from "./components/alarm";
+import Setting from "./components/setting";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/team-create" element={< TeamCreate />} />
-          <Route path="/login" element={ <Login/> }/>
-        </Routes>
-      </Router>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/team-create" element={<TeamCreate />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/mypage" element={<MyPage />} />
+
+        {/* 🔥 추가된 라우트 */}
+        <Route path="/alarm" element={<Alarm />} />
+        <Route path="/setting" element={<Setting />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
